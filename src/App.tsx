c0ee6bb4b6655052a764e2b7c29857b7460b5527
yuumillar <araera111@gmail.com>
2022-11-13 23:43:32 +0900

    feat:

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 
-const App = () => <div className="mx-auto container p-4 m-4 rounded shadow bg-blue-200">aaa</div>;
-
-export default App;
+export const App = () => {
+  const a = '';
+  return <div>App</div>;
+};
